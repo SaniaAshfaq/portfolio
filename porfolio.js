bf1b1a2d11123f12689a82fd3c45b8f83ee6abe1
SaniaAshfaq <saniaashfaq16860@gmail.com>
2024-06-28 22:46:02 +0500

    changed

--- porfolio.js
+++ porfolio.js
@@ -1,56 +1,56 @@
 
-let responsive = document.querySelector('.responsive');
-let newdiv;
-responsive.addEventListener('click' ,function(){
-    responsive.style.display = 'none';
-    console.log('clicked');
-  newdiv = document.createElement('div');
-  newdiv.classList.add('jsClass');
-  newdiv.innerHTML = ` <ion-icon name="close-outline" class="responsive jsresponsive"></ion-icon>
-  <a href="#mycontent" class="home jslink" >Home</a>
-  <a href="#about" class='jslink'>About</a>
-  <a href="#services" class='jslink'>services</a>
-  <a href="#projects" class='jslink'>projects</a>
+// let responsive = document.querySelector('.responsive');
+// let newdiv;
+// responsive.addEventListener('click' ,function(){
+//     responsive.style.display = 'none';
+//     console.log('clicked');
+//   newdiv = document.createElement('div');
+//   newdiv.classList.add('jsClass');
+//   newdiv.innerHTML = ` <ion-icon name="close-outline" class="responsive jsresponsive"></ion-icon>
+//   <a href="#mycontent" class="home jslink" >Home</a>
+//   <a href="#about" class='jslink'>About</a>
+//   <a href="#services" class='jslink'>services</a>
+//   <a href="#projects" class='jslink'>projects</a>
   
-  `;
-   document.body.appendChild(newdiv);
+//   `;
+//    document.body.appendChild(newdiv);
 
-   newdiv.offsetHeight;
-   newdiv.style.left = '0';
+//    newdiv.offsetHeight;
+//    newdiv.style.left = '0';
 
-   let jsDiv = newdiv.querySelector('.responsive');
-jsDiv.addEventListener('click',function(){
-    newdiv.style.left = '-120%';
-    // newdiv.style.display = 'none';
-    responsive.style.display = 'block';
+//    let jsDiv = newdiv.querySelector('.responsive');
+// jsDiv.addEventListener('click',function(){
+//     newdiv.style.left = '-120%';
+//     // newdiv.style.display = 'none';
+//     responsive.style.display = 'block';
     
-});
-let link = newdiv.querySelectorAll('.jslink');
-link.forEach((event)=>{
-   event.addEventListener('click',function(){
-    newdiv.style.left = '-120%';
-    // newdiv.style.display='none';
-    responsive.style.display = 'block';
-   })
-})
+// });
+// let link = newdiv.querySelectorAll('.jslink');
+// link.forEach((event)=>{
+//    event.addEventListener('click',function(){
+//     newdiv.style.left = '-120%';
+//     // newdiv.style.display='none';
+//     responsive.style.display = 'block';
+//    })
+// })
     
-});
-let hideproj = document.querySelector(".hided");
-let shoBtn = document.querySelector(".Showmore");
+// });
+// let hideproj = document.querySelector(".hided");
+// let shoBtn = document.querySelector(".Showmore");
 
-shoBtn.addEventListener("click", function() {
-    if (shoBtn.innerHTML === "Show More") {
-        hideproj.style.display = "block";
-        shoBtn.innerHTML = "Show Less";
-    } else {
-        hideproj.style.display = "none";
-        shoBtn.innerHTML = "Show More";
-    }
+// shoBtn.addEventListener("click", function() {
+//     if (shoBtn.innerHTML === "Show More") {
+//         hideproj.style.display = "block";
+//         shoBtn.innerHTML = "Show Less";
+//     } else {
+//         hideproj.style.display = "none";
+//         shoBtn.innerHTML = "Show More";
+//     }
 
 
 
 
     
 
-});
+// });
 
